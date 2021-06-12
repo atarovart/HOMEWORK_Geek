03d8c448f25780bd3b545bbc4ca997e6ddda228e
Arthur <83765685+atarovart@users.noreply.github.com>
2021-06-12 21:36:15 +0700

    Add files via upload

---
+++ Lesson_5/Lesson_5_task_1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//task_1
+void PrintArray(double arr[], int size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << arr[i] << " " ;
+	}
+}
+
+int main()
+{
+	const int mysize = 3;
+	double myarr[] = { 1.2, 3.3, 5.9 };
+	PrintArray(myarr, mysize);
+	return 0;
+}
