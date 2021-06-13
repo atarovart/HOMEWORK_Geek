0aac679078ab978deae5585f8793c32b33407054
Arthur <83765685+atarovart@users.noreply.github.com>
2021-06-13 16:38:26 +0700

    Add files via upload

---
+++ Lesson_5/Lesson_5_task_3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//task_3
+void PrintArray(int arr[], int size)
+{
+	for (size_t a = 1; a <=22; a += 3)
+	{
+		cout << arr[a] << " " ;
+	}
+}
+
+int main()
+{
+	const int mysize = 8;
+	int myarr[1] = { 1 };
+	for (myarr[1])
+
+	PrintArray(myarr, mysize);
+	return 0;
+}
