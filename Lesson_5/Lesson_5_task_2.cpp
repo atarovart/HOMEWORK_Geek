48f150a592ad840b609b8a705a609faf77cb61c1
Arthur <83765685+atarovart@users.noreply.github.com>
2021-06-13 16:08:15 +0700

    Add files via upload

---
+++ Lesson_5/Lesson_5_task_2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void arrayprint(int arr[], size_t size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
+
+int arrayzeroone(int arr[], size_t size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		arr[i] = (arr[i] == 1) ? 0 : 1;
+	}
+	return 0;
+}
+int main()
+{
+	const int mysize = 5;
+	int myarr[] = { 1, 0, 1, 1, 0, 1 };
+	arrayprint(myarr, mysize);
+	arrayzeroone(myarr, mysize);
+	cout << endl;
+	return 0;
+}
+
+
