1d216560371e367f7be1d7e2b26ce309a2cf282a
Arthur <83765685+atarovart@users.noreply.github.com>
2021-06-20 22:32:33 +0700

    Add files via upload
    Сделал цикл, чтобы можно было смотреть много раз результат, но не смог сделать кнопку для завершения программы, и исключения символьных значений.. времени совсем нет(

---
+++ Lesson_7/Lesson_7_task_2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+#define IN_RANGE(a, b) ((a >= 0 && a < b) ? "true" : "false")
+using namespace std;
+int main()
+{
+	int number;
+	char q = 'q';
+	for (; ;)
+	{
+		cout << "Enter a number: " << endl;
+		cin >> number;
+		cout << IN_RANGE(number, 10) << endl;
+	}
+	return 0;
+}
