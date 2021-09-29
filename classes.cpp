7d88eb93eb3b3ea619d48f0776ac913df168d0b1
ARTUR <at.art7100@yandex.ru>
2021-09-29 21:43:27 +0300

    master

---
+++ classes.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Power
+{
+public:
+	void setPower(int a, int b)
+	{
+		m_a = a;
+		m_b = b;
+		m_c = pow(a, b);
+	}
+	int calculate()
+	{
+		cout << m_c << endl;
+		return m_c;
+	}
+
+private:
+	int m_a;
+	int m_b;
+	int m_c;
+};
+
+int main()
+{
+	Power power;
+	power.setPower(4, 3);
+	power.calculate();
+	return 0;
+}
